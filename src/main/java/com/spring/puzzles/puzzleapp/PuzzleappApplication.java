8ff21ee59101d69cbdef1e169f8f8fdfd4fbaa43
sshaparenko <zshaparenkoz@gmail.com>
2023-06-19 17:23:00 +0300

    initial commit

---
+++ src/main/java/com/spring/puzzles/puzzleapp/PuzzleappApplication.java
@@ -0,0 +1,13 @@
+package com.spring.puzzles.puzzleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/puzzles/puzzleapp/PuzzleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.puzzles.puzzleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuzzleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
