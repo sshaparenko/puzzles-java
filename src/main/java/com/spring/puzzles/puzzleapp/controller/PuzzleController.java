e761775cd6c00311c181b36cabc4d05afe5c115d
sshaparenko <zshaparenkoz@gmail.com>
2023-06-25 23:06:39 +0300

    - change UploadImage controller realization - add autoSolve controller

--- src/main/java/com/spring/puzzles/puzzleapp/controller/PuzzleController.java
+++ src/main/java/com/spring/puzzles/puzzleapp/controller/PuzzleController.java
@@ -1,38 +1,55 @@
 package com.spring.puzzles.puzzleapp.controller;
 
 import com.spring.puzzles.puzzleapp.common.ApiResponse;
+import com.spring.puzzles.puzzleapp.service.PuzzleAutoSolve;
 import com.spring.puzzles.puzzleapp.service.PuzzleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
 public class PuzzleController {
     private final PuzzleService puzzleService;
+    private final PuzzleAutoSolve autoSolve;
+
     @PostMapping("/upload")
-    public ResponseEntity<ApiResponse> uploadImage (@RequestParam MultipartFile image) {
-        try {
-            puzzleService.saveImage(image);
-            return new ResponseEntity<>(new ApiResponse(true, "Image was uploaded successfully!"), HttpStatus.CREATED);
-        } catch (IOException e) {
-            return new ResponseEntity<>(new ApiResponse(false, e.toString()), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public String uploadImage (@RequestParam MultipartFile image) {
+        if (image.isEmpty()) return "redirect:/no-file.html";
+        puzzleService.saveImage(image);
+        return "redirect:/index.html";
     }
+
     @GetMapping("/puzzle")
     public ResponseEntity<ApiResponse> getPuzzle (@RequestParam Integer rows, @RequestParam Integer columns, @RequestParam String filename) {
         try {
-            puzzleService.setImage("src/main/resources/static/images/" + filename);
+            puzzleService.setImage(filename);
             String json = puzzleService.savePuzzles(rows, columns, filename);
-            System.out.println(json);
             return new ResponseEntity<>(new ApiResponse(true, json), HttpStatus.OK);
         } catch (IOException e) {
             return new ResponseEntity<>(new ApiResponse(false, "The IO Exception has been occurred!"), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/auto-solve")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> autoSolve(@RequestParam String imageName) {
+        try {
+            InputStream result = autoSolve.solve(imageName);
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(result));
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
