740594a2824caf5cd3b42e4f7ce5e55ad32c79fb
sshaparenko <zshaparenkoz@gmail.com>
2023-06-27 23:48:39 +0300

    - change the comparison algorithm to compare puzzle with original image area rather than create new image from puzzle and compare it with the whole original image

--- src/main/java/com/spring/puzzles/puzzleapp/service/PuzzleAutoSolve.java
+++ src/main/java/com/spring/puzzles/puzzleapp/service/PuzzleAutoSolve.java
@@ -11,9 +11,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- *
  * The puzzle auto-solve algorithm.
- *
+ * <p>
  * <br/>Class has six methods:
  * <ul>
  *     <li>getDifferance() - calculate the difference
@@ -26,15 +25,13 @@ import java.util.stream.Collectors;
  *     <li></li>
  *     <li></li>
  * </ul>
- *
- * */
+ */
 
 @Component
 @Slf4j
 public class PuzzleAutoSolve {
 
     /**
-     *
      * @param originalImageName name of the original image
      * @return InputStream of bytes of the result image
      * @throws IOException
@@ -59,15 +56,14 @@ public class PuzzleAutoSolve {
 
         Map<Integer, BufferedImage> resultMap = new HashMap<>();
         Iterator<BufferedImage> iterator = filesList.iterator();
-        while(iterator.hasNext()) {
+        while (iterator.hasNext()) {
             int count = 0;
             int min_count = 0;
             double min_differance = 100.0;
             BufferedImage puzzle = iterator.next();
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j < columns; j++) {
-                    BufferedImage imageToCompare = createImageToCompare(puzzle, originalImage.getWidth(), originalImage.getHeight(), j, i);
-                    double current_differance = getDifference(imageToCompare, originalImage);
+                    double current_differance = getDifference(puzzle, originalImage, i, j);
                     if (min_differance > current_differance) {
                         min_differance = current_differance;
                         min_count = count;
@@ -78,7 +74,7 @@ public class PuzzleAutoSolve {
             resultMap.put(min_count, puzzle);
             iterator.remove();
         }
-
+        System.out.println(resultMap);
         BufferedImage resultImage = createImage(originalImage.getWidth(), originalImage.getHeight(), resultMap);
         //saveImage(resultImage);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -92,10 +88,10 @@ public class PuzzleAutoSolve {
     }
 
     /**
-     *
      * Generates the result image from puzzles dictionary (index, puzzle)
-     * @param width total width
-     * @param height total height
+     *
+     * @param width    total width
+     * @param height   total height
      * @param filesMap dictionary of puzzles and indexes
      * @return image generated from puzzles
      */
@@ -123,32 +119,8 @@ public class PuzzleAutoSolve {
     }
 
     /**
-     *
-     * Generates an image from the specified puzzle with width and height equal to the values of original image.
-     * row and column parameters are used to set the position of the puzzle on image.
-     * As a result, this method generates an image with width and height of the original image and puzzle on it,
-     * which will change its position according to the values of row and colum.
-     * So, in each iteration we will have a puzzle set to its position and a black space around it.
-     * @param tile BufferedImage of puzzle
-     * @param width total width of the image
-     * @param height total height of the image
-     * @param column value of the column
-     * @param row value of the row
-     * @return generated image
-     */
-    private BufferedImage createImageToCompare(BufferedImage tile, int width, int height, int column, int row) {
-        BufferedImage resultImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-        int puzzleHeight = tile.getHeight();
-        int puzzleWidth = tile.getWidth();
-        Graphics2D g2d = resultImage.createGraphics();
-        g2d.drawImage(tile, puzzleWidth * column, puzzleHeight * row, puzzleWidth, puzzleHeight, null);
-        g2d.dispose();
-        return resultImage;
-    }
-
-    /**
-     *
      * Save the image to resources
+     *
      * @param image image to save
      */
     private void saveImage(BufferedImage image) {
@@ -163,8 +135,8 @@ public class PuzzleAutoSolve {
     }
 
     /**
-     *
      * Access images of puzzles in the specified dir and put them into List.
+     *
      * @param dirPath path do the directory, where all the puzzles stored
      * @return list of puzzles of type BufferedImage
      */
@@ -191,22 +163,16 @@ public class PuzzleAutoSolve {
     }
 
     /**
-     *
      * Calculates the difference between images (original and generated). This difference is used in the solve() method
      * in order to find the position of the puzzle.
+     *
      * @param imageToCompare generated image, that should be compared with the original
-     * @param original original image, that should be compared with the generated one
+     * @param original       original image, that should be compared with the generated one
      * @return percentage of difference
      */
-    private double getDifference(BufferedImage imageToCompare, BufferedImage original) {
+    public double getDifference(BufferedImage imageToCompare, BufferedImage original, int row, int col) {
         int width1 = imageToCompare.getWidth();
         int height1 = imageToCompare.getHeight();
-        int width2 = original.getWidth();
-        int height2 = original.getHeight();
-
-        if (width1 != width2 || height1 != height2) {
-            throw new IllegalArgumentException("Images must have the same dimensions");
-        }
 
         int numPixels = width1 * height1;
         int diffCount = 0;
@@ -214,7 +180,7 @@ public class PuzzleAutoSolve {
         for (int y = 0; y < height1; y++) {
             for (int x = 0; x < width1; x++) {
                 int rgb1 = imageToCompare.getRGB(x, y);
-                int rgb2 = original.getRGB(x, y);
+                int rgb2 = original.getRGB(col * width1 + x, row * height1 + y);
 
                 Color color1 = new Color(rgb1);
                 Color color2 = new Color(rgb2);
